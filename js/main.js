f4d048d768ec789d0e2584bc8165e827af384a9e
hiroakioike <bbt76aki@gmail.com>
2024-06-14 13:53:31 +0900

    ヘッダー修正

--- js/main.js
+++ js/main.js
@@ -1,18 +1,15 @@
-// js/main.js
-document.addEventListener("DOMContentLoaded", function () {
-  const humbargerOpen = document.querySelector(".humbarger-open");
-  const humbargerClose = document.querySelector(".humbarger-close");
-  const navList = document.querySelector(".header-nav-list");
+const hamburgerOpen = document.getSelectedById(".hamburger-open");
+const hamburgerClose = document.getSelectedById(".hamburger-close");
+const navList = document.getSelectedById(".header__nav--list");
 
-  humbargerOpen.addEventListener("click", function () {
-    navList.classList.toggle("active");
-    humbargerOpen.style.display = "none";
-    humbargerClose.style.display = "block";
-  });
+hamburgerOpen.addEventListener("click", function () {
+  navList.classList.toggle("active");
+  hamburgerOpen.style.display = "none";
+  hamburgerClose.style.display = "block";
+});
 
-  humbargerClose.addEventListener("click", function () {
-    navList.classList.toggle("active");
-    humbargerOpen.style.display = "block";
-    humbargerClose.style.display = "none";
-  });
+hamburgerClose.addEventListener("click", function () {
+  navList.classList.toggle("active");
+  hamburgerOpen.style.display = "block";
+  hamburgerClose.style.display = "none";
 });
