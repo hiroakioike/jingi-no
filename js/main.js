9de95e07e2f35162d67e7862bdad4e7358d6b8b1
hiroakioike <bbt76aki@gmail.com>
2024-06-19 17:24:29 +0900

    WIP.フッター修正

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const hamburgerOpen = document.getElementById("hamburgerOpen");
+const hamburgerClose = document.getElementById("hamburgerClose");
+const navList = document.getElementById("headerNavList");
+
+hamburgerOpen.addEventListener("click", function () {
+  navList.classList.toggle("active");
+  hamburgerOpen.style.display = "none";
+  hamburgerClose.style.display = "block";
+});
+
+hamburgerClose.addEventListener("click", function () {
+  navList.classList.toggle("active");
+  hamburgerOpen.style.display = "block";
+  hamburgerClose.style.display = "none";
+});
+
+const policy = document.getElementById("textPolicy");
+policy.addEventListener("click", function () {
+  alert("ルールとマナーを守って楽しくデュエル!!");
+});
