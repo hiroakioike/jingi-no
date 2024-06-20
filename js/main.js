c63394e066bf41e77bcdd511b1320baa2eb162d2
hiroakioike <bbt76aki@gmail.com>
2024-06-20 21:49:03 +0900

    コメントアウトとか

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-// const hamburgerOpen = document.getElementById("hamburgerOpen");
-// const hamburgerClose = document.getElementById("hamburgerClose");
-// const navList = document.getElementById("headerNavList");
+const hamburgerOpen = document.getElementById("hamburgerOpen");
+const hamburgerClose = document.getElementById("hamburgerClose");
+const navList = document.getElementById("headerNavList");
 
-// hamburgerOpen.addEventListener("click", function () {
-//   navList.classList.toggle("active");
-//   hamburgerOpen.style.display = "none";
-//   hamburgerClose.style.display = "block";
-// });
+hamburgerOpen.addEventListener("click", function () {
+  navList.classList.toggle("active");
+  hamburgerOpen.style.display = "none";
+  hamburgerClose.style.display = "block";
+});
 
-// hamburgerClose.addEventListener("click", function () {
-//   navList.classList.toggle("active");
-//   hamburgerOpen.style.display = "block";
-//   hamburgerClose.style.display = "none";
-// });
+hamburgerClose.addEventListener("click", function () {
+  navList.classList.toggle("active");
+  hamburgerOpen.style.display = "block";
+  hamburgerClose.style.display = "none";
+});
 
 const policy = document.getElementById("textPolicy");
 policy.addEventListener("click", function () {
