7769059bd970fce0c88f68c8b5ac1b78092a0cac
hiroakioike <bbt76aki@gmail.com>
2024-06-21 16:35:47 +0900

    NEWS追加

--- js/main.js
+++ js/main.js
@@ -1,22 +1,17 @@
-const hamburgerMenu = document.getElementById('hamburgerMenu');
-const overlay = document.getElementById('headerNavOverlay');
-const navList = document.getElementById('headerNavList');
+const hamburgerMenu = document.getElementById("hamburgerMenu");
+const overlay = document.getElementById("headerNavOverlay");
+const navList = document.getElementById("headerNavList");
 
-hamburgerMenu.addEventListener('click', function () {
-  hamburgerMenu.classList.toggle('active');
-  navList.classList.toggle('active');
-  overlay.classList.toggle('active');
+hamburgerMenu.addEventListener("click", function () {
+  hamburgerMenu.classList.toggle("active");
+  navList.classList.toggle("active");
+  overlay.classList.toggle("active");
 });
 
-overlay.addEventListener('click', function () {
-  hamburgerMenu.classList.remove('active');
-  navList.classList.remove('active');
-  overlay.classList.remove('active');
-});
-
-const policy = document.getElementById('textPolicy');
-policy.addEventListener('click', function () {
-  alert('ルールとマナーを守って楽しくデュエル!!');
+overlay.addEventListener("click", function () {
+  hamburgerMenu.classList.remove("active");
+  navList.classList.remove("active");
+  overlay.classList.remove("active");
 });
 
 const policy = document.getElementById("textPolicy");
